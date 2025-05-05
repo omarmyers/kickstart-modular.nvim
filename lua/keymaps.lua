a891b875d42aa1d561eac6c544242fe16756ae80
Omar Myers <98858405+omarmyers@users.noreply.github.com>
2025-05-05 01:38:25 -0500

    chore: uncomment window keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -32,9 +32,9 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Window commands
---vim.keymap.set('n', '<leader>-', '<C-W>s', { desc = 'Split Window Below', remap = true })
---vim.keymap.set('n', '<leader>|', '<C-W>v', { desc = 'Split Window Right', remap = true })
---vim.keymap.set('n', '<leader>wd', '<C-W>c', { desc = 'Close Window', remap = true })
+vim.keymap.set('n', '<leader>-', '<C-W>s', { desc = 'Split Window Below', remap = true })
+vim.keymap.set('n', '<leader>|', '<C-W>v', { desc = 'Split Window Right', remap = true })
+vim.keymap.set('n', '<leader>wd', '<C-W>c', { desc = 'Close Window', remap = true })
 
 -- Add undo break-points
 vim.keymap.set('i', ',', ',<c-g>u')
