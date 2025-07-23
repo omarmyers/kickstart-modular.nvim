8c3067ebea1fb4d84ea5411629a0bee68b644c92
Omar Myers <Omar.Myers@torchtechnologies.com>
2025-07-23 15:09:54 -0500

    style: use catppuccin built in styles for buffer-lin

--- lua/custom/plugins/buffer-line.lua
+++ lua/custom/plugins/buffer-line.lua
@@ -1,57 +1,16 @@
 return {
   {
     'akinsho/bufferline.nvim',
-    version = '*',
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    opts = {
-      options = {
-        diagnostics = 'nvim_lsp',
-        offsets = {
-          {
-            filetype = 'snacks_picker_list',
-            text = 'Snacks Explorer',
-            highlight = 'Directory',
-            text_align = 'center',
-          },
+    after = 'catppuccin',
+    config = function()
+      require('bufferline').setup {
+        highlights = require('catppuccin.groups.integrations.bufferline').get(),
+        options = {
+          diagnostics = 'nvim_lsp',
+          show_buffer_close_icons = false,
+          numbers = 'ordinal',
         },
-        separator_style = 'slant',
-        -- indicator = {
-        --   style = 'underline',
-        -- },
-        numbers = 'ordinal',
-      },
-      highlights = {
-        separator = {
-          fg = '#11111B', -- Custom color for the separator
-          -- bg = '#11111B',
-        },
-        separator_selected = {
-          fg = '#11111B', -- Color of separator in the active tab
-          -- bg = '#11111B',
-        },
-        separator_visible = {
-          fg = '#11111B',
-          -- bg = '#11111B',
-        },
-        buffer_selected = {
-          fg = '#B4BEFE',
-          -- bg = '#1E1E2E',
-        },
-        -- background = {
-        --   bg = '#181825',
-        -- },
-        fill = {
-          bg = '#11111B', -- Color of the tabline background
-        },
-        -- indicator_selected = {
-        --   fg = '#F38BA8',
-        --   bg = '#F38BA8',
-        -- },
-        -- indicator_visible = {
-        --   fg = '#F38BA8',
-        --   bg = '#F38BA8',
-        -- },
-      },
-    },
+      }
+    end,
   },
 }
--- lua/custom/plugins/color-theme.lua
+++ lua/custom/plugins/color-theme.lua
@@ -2,13 +2,62 @@ return {
   {
     'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    -- lazy = false,
+    lazy = false,
+    name = 'catppuccin-mocha',
     config = function()
       require('catppuccin').setup {
         flavour = 'mocha',
-        transparent_background = true,
+        transparent_background = false,
+        integrations = {
+          aerial = true,
+          alpha = true,
+          cmp = true,
+          dashboard = true,
+          flash = true,
+          fzf = true,
+          grug_far = true,
+          gitsigns = true,
+          headlines = true,
+          illuminate = true,
+          indent_blankline = { enabled = true },
+          leap = true,
+          lsp_trouble = true,
+          mason = true,
+          markdown = true,
+          mini = true,
+          native_lsp = {
+            enabled = true,
+            underlines = {
+              errors = { 'undercurl' },
+              hints = { 'undercurl' },
+              warnings = { 'undercurl' },
+              information = { 'undercurl' },
+            },
+          },
+          navic = { enabled = true, custom_bg = 'lualine' },
+          neotest = true,
+          neotree = true,
+          noice = true,
+          notify = true,
+          semantic_tokens = true,
+          snacks = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+          which_key = true,
+        },
+        specs = {
+          {
+            'akinsho/bufferline.nvim',
+            optional = true,
+            opts = function(_, opts)
+              if (vim.g.colors_name or ''):find 'catppuccin' then
+                opts.highlights = require('catppuccin.groups.integrations.bufferline').get()
+              end
+            end,
+          },
+        },
       }
-      vim.cmd.colorscheme 'catppuccin'
     end,
   },
   {
